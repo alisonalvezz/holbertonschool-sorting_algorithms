d4d52efc765fe1ac3c4c129100dde7bb9bf09552
JorgeBeritan <inaikmcontacto@gmail.com>
2024-04-08 11:40:32 -0300

    Fix Betty Warnings

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,9 +6,9 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, aux;
 
-	if(array == NULL)
+	if (array == NULL)
 		return;
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{

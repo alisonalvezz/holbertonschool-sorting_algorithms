f4d57e5fe8ba17c193e8cbcc46d2e689c11067e6
Alison <8661@holbertonstudents.com>
2024-04-10 13:37:02 +0000

    hope it works

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,53 +1,37 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of ints in ascending order
- * @list: pointer to the head of the doubly linked list
+ * sorts a doubly linked list of ints in ascending order
+ * @list: list
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
-	listint_t *temp;
-	listint_t *next;
+	listint_t *node = NULL, *aux = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	current = (*list)->next;
-	temp = NULL;
-	next = NULL;
-
-	while (current != NULL)
+	node = *list;
+	node = node->next;
+	while (node)
 	{
-		temp = current->prev;
-		next = current->next;
-		while (temp != NULL && temp->n > current->n)
-			{
-				temp = temp->prev;
-			}
-		if (temp == NULL)
+		while (node->prev && node->n < (node->prev)->n)
 		{
-			current->prev->next = next;
-			if (next != NULL)
-				next->prev = current->prev;
-			current->prev = NULL;
-			current->next = *list;
-			(*list)->prev = current;
-			*list = current;
+			aux = node;
+			if(node->next)
+				(node->next)->prev = aux->prev;
+			(node->prev)->next = aux->next;
+			node = node->prev;
+			aux->prev = node->prev;
+			aux->next = node;
+			if (node->prev)
+				(node->prev)->next = aux;
+			node->prev = aux;
+			if (aux->prev == NULL)
+				*list = aux;
+			print_list(*list);
+			node = node->prev;
 		}
-		else
-		{
-			current->prev->next = next;
-			if (next != NULL)
-				next->prev = current->prev;
-			current->prev = temp;
-			current->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = current;
-			temp->next = current;
-		}
-		print_list(*list);
-		current = next;
+		node = node->next;
 	}
-	print_list(*list);
 }

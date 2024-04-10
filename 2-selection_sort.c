4037327300e09c416f59ece151989991a6762f97
Alison <8661@holbertonstudents.com>
2024-04-10 13:10:27 +0000

    betty now works

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 
+/**
+ * selection_sort - sorts an array of ints in ascending order
+ * @array: array to sort
+ * @size: size of the array
+ */
+
 void selection_sort(int *array, size_t size)
 {
 	int aux = 0;

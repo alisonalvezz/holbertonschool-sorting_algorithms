55a7eb713515ac89493a1c93c50c41aa0c47c721
JorgeBeritan <inaikmcontacto@gmail.com>
2024-04-10 06:13:12 -0300

    Selectio sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	int aux = 0;
+	unsigned int i, j = 0, min = 0;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for(i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if(array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			aux = array[i];
+			array[i] = array[min];
+			array[min] = aux;
+			print_array(array, size);
+		}
+	}
+}

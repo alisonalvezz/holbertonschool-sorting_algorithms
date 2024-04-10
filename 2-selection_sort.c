c6fc6f88482409a0248c4ffb49878af4692b68d5
JorgeBeritan <inaikmcontacto@gmail.com>
2024-04-10 06:16:08 -0300

    Fix Betty Warnings

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,12 +8,12 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if(array[j] < array[min])
+			if (array[j] < array[min])
 				min = j;
 		}
 		if (min != i)
